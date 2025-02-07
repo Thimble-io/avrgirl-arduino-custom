5ac4442e48bbbfa81eaed97fa8180d9a871378e9
harindum-xino <harindum@xinotechsoftware.com>
2025-02-07 14:08:37 +0530

    More browser support & port forwarding #227 changes added

--- avrgirl-arduino.js
+++ avrgirl-arduino.js
@@ -15,6 +15,7 @@ var injectDependencies = function(boards, Connection, protocols) {
       megaDebug: opts.megaDebug || false,
       board: opts.board || 'uno',
       port: opts.port || '',
+      serialPort: opts.serialPort || null,
       manualReset: opts.manualReset || false,
       disableVerify: opts.disableVerify || false
     };
@@ -148,4 +149,3 @@ var injectDependencies = function(boards, Connection, protocols) {
 };
 
 module.exports = injectDependencies;
-
